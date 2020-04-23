ef045d97a1ee9099f617bd98d0d7814dc7541aa6
Demetrio Battaglia <deme3.iot@gmail.com>
2020-04-23 15:25:14 +0200

    Live dictionary implementation (dict.cc via cors-anywhere)

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -22,6 +22,20 @@ if (typeof w === 'object') {
     if (typeof kObj.ACRExtensions === 'undefined') {
         kObj.ACRExtensions = true;
         var oldMethod = kObj.show;
+        var oldHideMethod = kObj.hide;
+
+
+        var listStyleExtra = document.createElement("style");
+        listStyleExtra.type = "text/css";
+        kDoc.head.appendChild(listStyleExtra);
+        listStyleExtra.sheet.insertRule("#dictionary-widget-ext a {text-decoration: none; color: black;}");
+        listStyleExtra.sheet.insertRule("#dictionary-widget-ext a:hover {text-decoration:underline;}");
+        listStyleExtra.sheet.insertRule("#dictionary-widget-ext kbd {color: #039; font-family: serif,arial; font-size: 10pt;}");
+        kObj.hide = function () {
+            var res = oldHideMethod.apply(kObj, arguments);
+            $("#dictionary-widget-ext", kDoc).remove();
+            return res;
+        };
         kObj.show = function () {
             var res = oldMethod.apply(kObj, arguments);
             var txtDoc = null;
@@ -61,6 +75,58 @@ if (typeof w === 'object') {
                     newW.document.body.appendChild(r.cloneContents());
                 }
             });
+
+            if (r) {
+                /*
+                <div class="ui-dialog ui-widget ui-widget-content ui-corner-all kindle_menu"
+                style="display: block; z-index: 1002; outline: 0px; top: 522px; left: 388px; bottom: auto;
+                height: 64px; width: auto;">
+                <div class="ui-dialog-content ui-widget-content" style="display:block; width:auto; min-height: 0px; height: auto">This is content</div></div>
+                */
+                /* r = selected text
+                // here we open the dictionary in-page pop-up*/
+                $("#dictionary-widget-ext", kDoc).remove();
+                var mainDialog = $(".ui-dialog.kindle_menu[role=dialog]", kDoc);
+                var selectedWords = r.cloneContents().children;
+                var targetPosition = mainDialog.position();
+
+                var dictionaryUIWidget = document.createElement("div");
+                dictionaryUIWidget.id = "dictionary-widget-ext";
+                dictionaryUIWidget.className = "ui-dialog ui-widget ui-widget-content ui-corner-all kindle_menu";
+                dictionaryUIWidget.style = "display: block; z-index: 1002; outline: 0px; max-height: 310px; top: " + (targetPosition.top - 320) + "px; left: " + (targetPosition.left) + "px; max-width:" + mainDialog.width() + "px";
+                var dictionaryUIWidgetContent = document.createElement("div");
+                dictionaryUIWidgetContent.className = "ui-dialog-content ui-widget-content";
+                dictionaryUIWidgetContent.style = "display: block; width: auto; min-height: 0px; max-height: 310px; overflow-y: auto; height: auto";
+
+                selectedWords = selectedWords[0]; /*TODO: add support for multiple words*/
+                $.get("https://cors-anywhere.herokuapp.com/pocket.dict.cc/?s=" + selectedWords.innerText, (data) => {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(data, "text/html");
+                    var definitions = doc.querySelectorAll("dt, dt + dd");
+                    console.log(doc, definitions);
+                    var list = document.createElement("ul");
+                    list.style = "background-color:#eee;padding:6px 14px;list-style-type:none;";
+                    dictionaryUIWidgetContent.innerText = "";
+                    for(var x = 0; x < definitions.length; x++) {
+                        if(definitions[x].tagName == "DD") {
+                            list.children[list.children.length-1].appendChild(definitions[x]);
+                        } else {
+                            var listElement = document.createElement("li");
+                            listElement.appendChild(definitions[x]);
+                            list.appendChild(listElement);
+                        }
+                    }
+                    var allLinks = list.getElementsByTagName("a");
+                    for(var x = 0; x < allLinks.length; x++) {
+                        allLinks[x].href = "http://pocket.dict.cc/" + new URL(allLinks[x].href).search;
+                        allLinks[x].target = "_blank";
+                    }
+                    dictionaryUIWidgetContent.appendChild(list);
+                });
+                dictionaryUIWidgetContent.innerText = "Loading...";
+                dictionaryUIWidget.appendChild(dictionaryUIWidgetContent);
+                mainDialog[0].insertAdjacentElement("beforebegin", dictionaryUIWidget);
+            }
 
             return res;
         };
